b4e031f87f8ad9041fe66e8e3d5196038ad50bc0
Rambod Azimi <59399542+rambodazimi@users.noreply.github.com>
2023-06-13 15:10:38 -0400

    Update form.py

--- form.py
+++ form.py
@@ -9,32 +9,15 @@ import threading
 import subprocess
 import pyautogui
 
-
 # Set the endpoint and key variables with the values from the Azure portal
-endpoint = "https://walterforms.cognitiveservices.azure.com/"
-key = "d436aeca36244041b2c3cf21bee5921b"
+endpoint = "<Your Endpoint>"
+key = "<Your Key>"
+
+model_id = "rambod-1"
+formUrl = "https://docs.google.com/uc?export=download&id=1g6kL0DxdjsK-J-c5d-iZZp9IyQlWXHid"
 
 # global variables
 csv_filep = "C:\\Users\\razimi\\OneDrive - Walter Surface Technologies\\Desktop\\python\\form_recognizer\\output_prebuilt.csv"
-
-def analyze_url():
-    url = url_entry.get()
-
-    # Start a new thread to perform the analysis
-    analysis_thread = threading.Thread(target=perform_analysis, args=(url,))
-    analysis_thread.start()
-    
-    try:
-        message_label.config(text="PDF file in the url has not been found!", fg="red")
-        response = requests.get(url)
-        if response.status_code == 200:
-            analyze_invoice(url, True, "noFile")
-            message_label.config(text="CSV file has been generated successfully!!", fg="green")
-        else:
-            message_label.config(text="URL is invalid or inaccessible.", fg="red")
-
-    except requests.exceptions.RequestException as e:
-        message_label.config(text="Invalid URL!", fg="red")
 
 def browse_pdf():
     message_label.config(text="Analyzing the document...", fg="blue")
@@ -44,16 +27,15 @@ def browse_pdf():
         # Start a new thread to perform the analysis
         analysis_thread = threading.Thread(target=perform_analysis, args=(file_path,))
         analysis_thread.start()
+        analyze_invoice_rambod("noURL", False, file_path)
         analyze_invoice("noURL", False, file_path)
         message_label.config(text="CSV file has been generated successfully!", fg="green")
         open_files_side_by_side(pdf_path=file_path, excel_path=csv_filep)
-
 
 def perform_analysis(input_data):
     # Simulate a long-running analysis task
     import time
     time.sleep(5)  # Replace this with your actual analysis code
-    
 
 # Create the main window
 window = tk.Tk()
@@ -73,23 +55,21 @@ content_frame.pack(pady=10)
 heading_label = tk.Label(content_frame, text="Form Recognizer", font=("Arial", 16, "bold"), bg="#F0F0F0")  # Set the background color of the heading label
 heading_label.pack()
 
-# Create a label and entry for the URL
-url_label = tk.Label(content_frame, text="Enter URL:", font=("Arial", 12), bg="#F0F0F0")  # Set the background color of the URL label
-url_label.pack(pady=5)
-url_entry = tk.Entry(content_frame, font=("Arial", 12))
-url_entry.pack(pady=5)
-
-# Create a button to trigger the analysis
-analyze_button = tk.Button(content_frame, text="Analyze", font=("Arial", 12), command=analyze_url)
-analyze_button.pack(pady=15)
+heading_label = tk.Label(content_frame, text="Choose a PDF file and wait for the analysis", font=("Arial", 10), bg="#F0F0F0")  # Set the background color of the heading label
+heading_label.pack()
 
 # Create a button to browse for a PDF file
-browse_button = tk.Button(content_frame, text="Browse", font=("Arial", 12), command=browse_pdf)
+browse_button = tk.Button(content_frame, text="Browse", font=("Arial", 15), command=browse_pdf)
 browse_button.pack(pady=10)
 
+def help_description():
+    desc = "This software processes PDF invoice files, extracting data\n and saving the results in a CSV format. To utilize this software,\nclick the \"Browse\" button, select your desired PDF file,\nand await completion of the analysis, which typically takes\n a few seconds. A confirmation message will be displayed\nupon successful completion of the analysis."
+    description_label = tk.Label(content_frame, text=desc, font=("Arial", 8), bg="#F0F0F0")
+    description_label.pack()
+
 # Create a button to browse for a PDF file
-# start_button = tk.Button(content_frame, text="Start", font=("Arial", 12))
-# start_button.pack(pady=5)
+help_button = tk.Button(content_frame, text="Help", font=("Arial", 10), command=help_description)
+help_button.pack(pady=5)
 
 # Create a message label
 message_label = tk.Label(window, text="", font=("Arial", 12), bg="#F0F0F0")  # Set the background color of the message label
@@ -122,7 +102,6 @@ def analyze_invoice(url_input, isUrl, filePath):
     output_file = "C:\\Users\\razimi\\OneDrive - Walter Surface Technologies\\Desktop\\python\\form_recognizer\\output_prebuilt.csv"
     file_exists = os.path.exists(output_file)
 
-
     with open(output_file, "a", newline="") as csv_file:
         writer = csv.writer(csv_file)
 
@@ -139,17 +118,16 @@ def analyze_invoice(url_input, isUrl, filePath):
                 invoices = poller.result()
 
         for idx, invoice in enumerate(invoices.documents):
+            """
             writer.writerow(["--------Recognizing invoice--------"])
 
             # Write field values and confidence scores to the CSV file
             fields = [
                 ("Customer Name", invoice.fields.get("VendorName")),
                 ("Customer Address", invoice.fields.get("VendorAddress")),
-                # ("Customer Address Recipient", invoice.fields.get("VendorAddressRecipient")),
                 ("Supplier Name", invoice.fields.get("CustomerName")),
                 ("Supplier Id", invoice.fields.get("CustomerId")),
                 ("Supplier Address", invoice.fields.get("CustomerAddress")),
-                # ("Supplier Address Recipient", invoice.fields.get("CustomerAddressRecipient")),
                 ("PO Number", invoice.fields.get("InvoiceId")),
                 ("PO Date", invoice.fields.get("InvoiceDate")),
                 ("Payment Term", invoice.fields.get("PaymentTerm")),
@@ -162,21 +140,13 @@ def analyze_invoice(url_input, isUrl, filePath):
                 ("Net Total", invoice.fields.get("SubTotal")),
                 ("PO Total", invoice.fields.get("InvoiceTotal")),
                 ("Service Start Date", invoice.fields.get("ServiceStartDate")),
-                ("Service End Date", invoice.fields.get("ServiceEndDate")),
-                # ("Total Tax", invoice.fields.get("TotalTax")),
-                # ("Previous Unpaid Balance", invoice.fields.get("PreviousUnpaidBalance")),
-                # ("Amount Due", invoice.fields.get("AmountDue")),
-                # ("Service Address", invoice.fields.get("ServiceAddress")),
-                # ("Service Address Recipient", invoice.fields.get("ServiceAddressRecipient")),
-                # ("Remittance Address", invoice.fields.get("RemittanceAddress")),
-                # ("Remittance Address Recipient", invoice.fields.get("RemittanceAddressRecipient"))
+                ("Service End Date", invoice.fields.get("ServiceEndDate"))
             ]
 
             for field_name, field in fields:
                 if field:
                     writer.writerow([field_name, field.value])
-                #else:
-                    #writer.writerow([field_name, "N/A"])
+            """
 
             writer.writerow(["Invoice items:"])
             for idx, item in enumerate(invoice.fields.get("Items").value):
@@ -209,8 +179,10 @@ def analyze_invoice(url_input, isUrl, filePath):
 
                 if item_date:
                     writer.writerow(["Date", item_date.value])
+
                 if item_tax:
                     writer.writerow(["Tax", item_tax.value])
+
                 if item_amount:
                     writer.writerow(["Extension", item_amount.value])
 
@@ -250,6 +222,58 @@ def open_files_side_by_side(pdf_path, excel_path):
     # Move and resize the Excel window
     pyautogui.getWindowsWithTitle('Microsoft Excel')[0].resizeTo(excel_window_width, excel_window_height)
     pyautogui.getWindowsWithTitle('Microsoft Excel')[0].moveTo(excel_window_x, excel_window_y)
+
+def analyze_invoice_rambod(url_input, isUrl, filePath):
+
+    if(len(url_input) == 0):
+        invoiceUrl = "https://docs.google.com/uc?export=download&id=1BnyEGi00M1vL5mYsViZj2bv5cIIKDRZN"
+        invoiceUrl2 = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-REST-api-samples/master/curl/form-recognizer/sample-invoice.pdf"
+        invoiceUrl3 = "https://docs.google.com/uc?export=download&id=1g6kL0DxdjsK-J-c5d-iZZp9IyQlWXHid"
+    else:
+        invoiceUrl = url_input
+    document_analysis_client = DocumentAnalysisClient(endpoint=endpoint, credential=AzureKeyCredential(key))
+
+    output_file = "C:\\Users\\razimi\\OneDrive - Walter Surface Technologies\\Desktop\\python\\form_recognizer\\output_prebuilt.csv"
+    file_exists = os.path.exists(output_file)
+
+    with open(output_file, "a", newline="") as csv_file:
+        writer = csv.writer(csv_file)
+
+        # if not file_exists:
+            # writer.writerow(["--------Recognizing invoice--------"])
+
+        if(isUrl):
+            poller = document_analysis_client.begin_analyze_document_from_url(model_id, formUrl)
+            invoices = poller.result()
+
+        else:
+            with open(filePath, "rb") as f:
+                poller = document_analysis_client.begin_analyze_document(model_id, document=f, locale="en-US")
+                invoices = poller.result()
+
+        for idx, invoice in enumerate(invoices.documents):
+            writer.writerow(["--------Recognizing invoice--------"])
+
+            # Write field values and confidence scores to the CSV file
+            fields = [
+                ("Customer", invoice.fields.get("Customer")),
+                ("PO Number", invoice.fields.get("PO Number")),
+                ("PO Date", invoice.fields.get("PO Date")),
+                ("Supplier", invoice.fields.get("Supplier")),
+                ("Deliver To", invoice.fields.get("Deliver To")),
+                ("Pick Date", invoice.fields.get("Pick Date")),
+                ("Terms", invoice.fields.get("Terms")),
+                ("Ship Via", invoice.fields.get("Ship Via")),
+                ("How Shipped", invoice.fields.get("How Shipped")),
+                ("Net Total", invoice.fields.get("Net Total")),
+                ("PO Total", invoice.fields.get("PO Total"))
+            ]
+
+            for field_name, field in fields:
+                if field:
+                    writer.writerow([field_name, field.value])
+
+            writer.writerow([])  # Write an empty row to separate invoices
 
 # main
 # Start the main loop
